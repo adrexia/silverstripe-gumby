d6c885b69d3da00d99dfb80583a7f38661069dc1
Sean Harvey <sean@silverstripe.com>
2013-01-17 11:51:21 +1300

    Remove fixed navbar as it causes too many issues and make the nav work without JS

--- js/express.js
+++ js/express.js
@@ -1,62 +1,23 @@
 (function($) {
 	$(document).ready(function() {
+		$('.nav-collapse').addClass('collapse');
 
-
-
-
-	    /*************************  Dynamic sticky nav   *********************************/
-
-
-		 // fix sub nav on scroll
-	    var $win = $(window), 
-	    	$nav = $('.navbar'), 
-	    	navTop = $('.navbar').length && $('.navbar').offset().top, 
-	    	isFixed = 0;
-
-	    	processScroll();
-
-	    $nav.on('click', function () {
-	      if (!isFixed) setTimeout(function () {  
-	      		$win.scrollTop($win.scrollTop() - 47); 
-	      }, 10);
+		$('.carousel').carousel({
+			interval: 8000,
+			pause: ''
 	    });
 
-    	$win.on('scroll', processScroll);
-
-	    function processScroll() {
-	      var i, scrollTop = $win.scrollTop();
-	      if (scrollTop >= navTop && !isFixed) {
-	        isFixed = 1;
-	        $nav.addClass('navbar-fixed-top');
-	        $('#layout').addClass('fixed-padding');
-	      } else if (scrollTop < navTop && isFixed) {
-	        isFixed = 0;
-	        $nav.removeClass('navbar-fixed-top');
-	        $('#layout').removeClass('fixed-padding');
-	      }
-	    }
-	    /*************************/
-
-	    $('.carousel').carousel({
-			interval: 8000,
-			pause: ""
-	    });  
-
-
-
-	    $('.carousel').on('click','#pause .btn',function(){
-	    	if($(this).closest('.carousel').hasClass('play')){
+	    $('.carousel').on('click','#pause .btn', function() {
+	    	if($(this).closest('.carousel').hasClass('play')) {
 	    		$(this).closest('.carousel').carousel('cycle').removeClass('play');
 	    		$(this).find('.icon').removeClass('icon-play-js').html('&#xf04c;');
-	    	}else{
+	    	} else {
 	    		$(this).closest('.carousel').carousel('pause').addClass('play');
 	    		$(this).find('.icon').addClass('icon-play-js').html('&#xf04b;');
 	    	}	    	
 	    });
 
-
-
-		$('.navbar a .showChildren').click(function(e){
+		$('.navbar a .showChildren').click(function(e) {
 			e.preventDefault();
 			var oldOpen = $('.navbar .open');		
 			$(this).closest('li').addClass('open');
@@ -64,12 +25,11 @@
 			return false;
 		});
 
-		$('form fieldset > div.field:odd').addClass("odd");
+		$('form fieldset > div.field:odd').addClass('odd');
 
-		$(".page-toggle .button").click(function(){
-			$(this).toggleClass("open");
-			$(this).closest('li').toggleClass("open");
+		$('.page-toggle .button').click(function() {
+			$(this).toggleClass('open');
+			$(this).closest('li').toggleClass('open');
 		});
-		
 	})
 })(jQuery);
