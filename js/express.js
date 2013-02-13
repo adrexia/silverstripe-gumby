0b6fc1f18eb5af6d4451adbf3f5b322c4ed11fc1
Sean Harvey <sean@silverstripe.com>
2013-02-13 14:20:09 +1300

    Removing carousel stuff

--- js/express.js
+++ js/express.js
@@ -12,21 +12,6 @@
 				$(this).siblings('button').attr('aria-expanded', 'true');
 			});
 
-		$('.carousel').carousel({
-			interval: 8000,
-			pause: ''
-	    });
-
-	    $('.carousel').on('click','#pause .btn', function() {
-	    	if($(this).closest('.carousel').hasClass('play')) {
-	    		$(this).closest('.carousel').carousel('cycle').removeClass('play');
-	    		$(this).find('.icon').removeClass('icon-play-js').html('&#xf04c;');
-	    	} else {
-	    		$(this).closest('.carousel').carousel('pause').addClass('play');
-	    		$(this).find('.icon').addClass('icon-play-js').html('&#xf04b;');
-	    	}	    	
-	    });
-
 		$('.navbar a .showChildren').click(function(e) {
 			e.preventDefault();
 			var oldOpen = $('.navbar .open');		
